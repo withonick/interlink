3a7ffab68aafbc6c323c7d7073da672be8253c3b
Advant <felikethis@gmail.com>
2023-11-06 12:58:42 +0600

    auth

---
+++ app/Models/Hobby.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hobby extends Model
+{
+    use HasFactory;
+
+    protected $guarded = false;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function users(){
+        return $this->belongsToMany(User::class, 'users_hobbies', 'hobby_id', 'user_id');
+    }
+}
